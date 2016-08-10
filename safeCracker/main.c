b3752c9135782deea0db5ff8241aeeb16f91fa91
Neil <vitalene@msu.edu>
2016-08-10 14:55:20 -0400

    Initial Commit

---
+++ safeCracker/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  safeCracker
+//
+//  Created by Neil Vitale on 8/10/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
